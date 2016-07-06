f2b5c8dd1850412ea488a4bf66e24354cf4cbfed
Yulia Krechet <yulia.krechet@gmail.com>
2016-07-06 17:59:22 +0400

    Finally found out what does function parameters for

--- js/application.js
+++ js/application.js
@@ -10,56 +10,45 @@ $(document).ready(function(){
 	//Почему нажатие на кнопку удаляет цену от предыдущего нажатия,
 	//если цена выставлена хардкодом через вставку параграфа?
 
-	function hideMedicines () {
-		$('.all-medicine-wrapper__medicine').hide();
-	}
+	function filterTag(tag) {			
+		$('.filters').on('click', tag, function() {	
+			$('.all-medicine-wrapper__medicine').hide();		
+			$('.medicine__tags').find(tag).closest('.all-medicine-wrapper__medicine').show();
+		});	
+	};
 
-	$('.filters').on('click', '.filter_antibiotic', function() {
-		hideMedicines();
-		$('.medicine__tags').find('.filter_antibiotic').closest('.all-medicine-wrapper__medicine').show();
-	});
-	$('.filters').on('click', '.filter_antivirus', function() {
-		hideMedicines();
-		$('.medicine__tags').find('.filter_antivirus').closest('.all-medicine-wrapper__medicine').show();
-	});
-	$('.filters').on('click', '.filter_antiseptic', function() {
-		hideMedicines();
-		$('.medicine__tags').find('.filter_antiseptic').closest('.all-medicine-wrapper__medicine').show();
-	});
-	$('.filters').on('click', '.filter_nose', function() {
-		hideMedicines();
-		$('.medicine__tags').find('.filter_nose').closest('.all-medicine-wrapper__medicine').show();
-	});
-	$('.filters').on('click', '.filter_throat', function() {
-		hideMedicines();
-		$('.medicine__tags').find('.filter_throat').closest('.all-medicine-wrapper__medicine').show();
-	});
-	$('.filters').on('click', '.filter_anti-pain', function() {
-		hideMedicines();
-		$('.medicine__tags').find('.filter_anti-pain').closest('.all-medicine-wrapper__medicine').show();
-	});
-	$('.filters').on('click', '.filter_spray', function() {
-		hideMedicines();
-		$('.medicine__tags').find('.filter_spray').closest('.all-medicine-wrapper__medicine').show();
-	});
-	$('.filters').on('click', '.filter_local-anti-pain', function() {
-		hideMedicines();
-		$('.medicine__tags').find('.filter_local-anti-pain').closest('.all-medicine-wrapper__medicine').show();
-	});
-	$('.filters').on('click', '.filter_anti-inflammatory', function() {
-		hideMedicines();
-		$('.medicine__tags').find('.filter_anti-inflammatory').closest('.all-medicine-wrapper__medicine').show();
-	});
-	$('.filters').on('click', '.filter_vasculomotor', function() {
-		hideMedicines();
-		$('.medicine__tags').find('.filter_vasculomotor').closest('.all-medicine-wrapper__medicine').show();
-	});
+	filterTag('.filter_antibiotic');
+	filterTag('.filter_antivirus');
+	filterTag('.filter_antiseptic');
+	filterTag('.filter_nose');
+	filterTag('.filter_throat');
+	filterTag('.filter_anti-pain');
+	filterTag('.filter_spray');
+	filterTag('.filter_local-anti-pain');
+	filterTag('.filter_anti-inflammatory');
+	filterTag('.filter_vasculomotor');
+	filterTag('.cancel-filter');
 
-	$('.cancel-filter').on('click', function() {
-		$('.all-medicine-wrapper__medicine').show();
-	});
+	// А сначала я для каждого клика отдельно написала, пока не поняла, как работают параметры функции...
+	// $('.cancel-filter').on('click', function() {
+	// 	$('.all-medicine-wrapper__medicine').show();
+	// });
 
-	$('.medicine-detailed__side-effect').on('click', '.more', function() {
-		$(this).find('ul').slideDown();
+	// Это я сначала без аякса спрятанный блок показывала
+	// $('.medicine-detailed__side-effect').on('click', 'button', function() {
+	// 	$(this).text('Свернуть');
+	// 	$(this).closest('.medicine-detailed__side-effect').find('ul').slideDown();
+	// });
+
+	$('.medicine-detailed__side-effect').on('click', 'button', function() {
+		$.ajax({
+			url: 'amoksiklav-side-effects.html',
+		}).done(function(response) {
+			$('.medicine-detailed__side-effect > p').html(response).slideDown();
+		}).fail(function(request, errorType, errorMessage) {
+			alert('Error: ' + errorType + errorMessage);
+		}).always(function (xhr, status) {
+		    $.fancybox.hideLoading(); // А когда она должна показаться-то?
+		});
 	});
 });
